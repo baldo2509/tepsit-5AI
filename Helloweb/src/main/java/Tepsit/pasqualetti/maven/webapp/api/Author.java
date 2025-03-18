f0b314ad0b289c1762e211af25eb0c6fecc5e2e7
baldo2509 <psq.alx@gmail.com>
2025-03-18 18:29:34 +0100

    added book and author class

---
+++ Helloweb/src/main/java/Tepsit/pasqualetti/maven/webapp/api/Author.java
@@ -0,0 +1,34 @@
+package Tepsit.pasqualetti.maven.webapp.api;
+
+import java.util.List;
+
+public class Author {
+	private long id;
+	private String name;
+	private String surname;
+	private List<Author> authors;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public List<Author> getAuthors() {
+		return authors;
+	}
+	public void setAuthors(List<Author> authors) {
+		this.authors = authors;
+	}
+}
